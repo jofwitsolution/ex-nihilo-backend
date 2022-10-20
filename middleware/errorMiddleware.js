10a13ed4123058fd70cd7e8963f0a07caadbbf52
Faleye Oluwafemi joseph <faleyeoluwafemi1@gmail.com>
2022-10-20 10:44:50 +0100

    few changes made

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -13,7 +13,6 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
